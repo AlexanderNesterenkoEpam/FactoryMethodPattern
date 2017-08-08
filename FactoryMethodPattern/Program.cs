c9778630326b0daade5f637e5a316a52a76d5906
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2017-08-08 21:37:27 +0300

    first commit

---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IDeveloperFactory factory = CreateDevBySpeciality("Java");
+			IDeveloper dev = factory.CreateDeveloper();
+
+			dev.writeCode();
+		}
+
+		public static IDeveloperFactory CreateDevBySpeciality(String speciality)
+		{
+			if (speciality.Equals("Java"))
+			{
+				return new JavaDeveloperFactory();
+			}
+			else if (speciality.Equals("Sharp"))
+			{
+				return new SharpDeveloperFactory();
+			}
+			else
+			{
+				throw new ArgumentException();
+			}
+		}
+	}
+}
---
+++ FactoryMethodPattern/SharpDeveloper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+	public class SharpDeveloper : IDeveloper
+	{
+		public void writeCode()
+		{
+			Console.WriteLine("Write C# code ...");
+		}
+
+	}
+}
